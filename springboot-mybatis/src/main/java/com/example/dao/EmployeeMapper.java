19fef4847306c8356377f3958d93b1845d5e9aa4
820627765 <820627765@qq.com>
2021-09-24 03:38:44 +0800

    'udpate'

--- springboot-mybatis/src/main/java/com/example/dao/EmployeeMapper.java
+++ springboot-mybatis/src/main/java/com/example/dao/EmployeeMapper.java
@@ -2,8 +2,10 @@ package com.example.dao;
 
 import com.example.entity.Employee;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface EmployeeMapper {
     Employee getEmployeeById(Integer id);
 }
